bbe360b0eb9b25518eebd00ae786e0b4b5201d40
최종호 <victor_77@naver.com>
2020-08-10 11:16:54 +0900

    hot loader and dev-server setting

--- 2.끝말잇기/client.jsx
+++ 2.끝말잇기/client.jsx
@@ -1,5 +1,8 @@
 const React = require("react");
 const ReactDom = require("react-dom");
 const WordRelay = require("./WordRelay");
+const { hot } = require("react-hot-loader/root");
 
-ReactDom.render(<WordRelay />, document.querySelector("#root"));
+const Hot = hot(WordRelay);
+
+ReactDom.render(<Hot />, document.querySelector("#root"));
